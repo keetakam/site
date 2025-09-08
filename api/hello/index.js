b021db28cedf255db17201bfdf03e9ac14135593
keetakam <keeta.kam@outlook.com>
2025-09-08 09:11:41 +0700

    edit

---
+++ api/hello/index.js
@@ -0,0 +1,8 @@
+module.exports = async function (context, req) {
+  context.log('HTTP trigger function processed a request.');
+  context.res = {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: { message: 'Hello from Azure Functions!' }
+  };
+};
